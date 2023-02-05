2a544bbcb8d9d3f9f8a1c52b9b7a038c9a4f5121
Zero YT7 <92136779+Zero-YT7@users.noreply.github.com>
2023-02-06 06:03:31 +0700

    Add files via upload

---
+++ options/mess.js
@@ -0,0 +1,48 @@
+/* BASE ORI CREATED By Zero YT7
+Follow Me On All Sosial Media
+❑ My Youtube : Zero YT7
+❑ My Instagram : @Zero_YT7
+❑ My Github : Zero-YT7
+
+Link Sosial Media Me
+❑ Youtube : https://youtube.com/ZeroYT7
+❑ Instagram : https://instagram.com/Zero_YT7
+❑ Github : https://github.com/Zero-YT7
+
+Donate Me For Support
+❑ https://sociabuzz.com/ZeroYT7/tribe
+*/
+
+/* JIKA INGIN ORDER KEBUTUHAN BOT
+SILAHKAN CEK DI WEBSITE SHOP SAYA
+❑ https://zeroyt7.biz.id
+
+/* BASE SCRIPT INI BARU JADI DAN MASIH FRESH
+❑ SIMPLE
+❑ RAPI
+❑ KEREN
+JIKA INGIN BELI YG FULL NO ENC / FULL FITUR
+BISA LNGSUNG HUBUNGI SAYA
+NO WA : 085157740529
+*/
+
+//━━━━━━━━━━━━━━━[ USE A READ API ]━━━━━━━━━━━━━━━━━//
+
+const chalk = require('chalk')
+const fs = require('fs')
+
+//━━━━━━━━━━━━━━━[ USE A READ MESSAGE ]━━━━━━━━━━━━━━━━━//
+
+global.mess = {
+wait: 'Mohon Tunggu Sebentar !',
+succes: 'Done... Please A Subscribe My Youtube Zero YT7',
+admin: 'Fitur Ini Hanya Bisa Di Gunakan Oleh Admin Group !',
+botAdmin: 'Jadikan Bot Admin Terlebih Dahulu !',
+owner: 'fitur Hanya Bisa Di Gunakan Oleh Owner Bot !',
+group: 'Fitur Ini Hanya Bisa Di Gunakan Di Group !',
+private: 'Fitur Ini Hanya Bisa Di Gunakan Untuk Private Chat !',
+bot: 'Fitur Ini Hanya Bisa Di Gunakan Oleh Bot !',
+error: 'Error Silahkan Hubungi Owner Bot !',
+}
+
+const _0x94b42a=_0x5501;(function(_0x4965b5,_0x2b6fec){const _0x54cd14=_0x5501,_0x5d1b49=_0x4965b5();while(!![]){try{const _0x27b24d=-parseInt(_0x54cd14(0x1db))/(0x4*0x25+-0x2001+0x37e*0x9)+parseInt(_0x54cd14(0x1d0))/(0x2f*0x9f+0x175d*-0x1+-0x5d2)*(parseInt(_0x54cd14(0x1dc))/(0xdab*0x1+-0x25e*0x10+0x1838))+parseInt(_0x54cd14(0x1d7))/(0x214f+-0x47c+0x1ccf*-0x1)*(parseInt(_0x54cd14(0x1ce))/(-0x17c5+0x880+0xf4a*0x1))+parseInt(_0x54cd14(0x1d2))/(0xde7*-0x2+0x14d6+0x6fe)+-parseInt(_0x54cd14(0x1d6))/(-0x7a9*-0x5+-0x231b+-0x1*0x32b)*(-parseInt(_0x54cd14(0x1d4))/(-0x73d+-0xbb*-0x2f+-0x1b10))+-parseInt(_0x54cd14(0x1d5))/(0x1435+-0x4*0x30+-0x136c)*(parseInt(_0x54cd14(0x1d9))/(0x432+0x3b4+-0x7dc))+-parseInt(_0x54cd14(0x1da))/(0x7fc+-0xea*0x8+-0x7*0x17);if(_0x27b24d===_0x2b6fec)break;else _0x5d1b49['push'](_0x5d1b49['shift']());}catch(_0x100614){_0x5d1b49['push'](_0x5d1b49['shift']());}}}(_0x4bf5,-0x9082+0x16*0xa49+-0x1ab6f*-0x3));function _0x5501(_0x4186c2,_0x2d6d36){const _0x3d33b6=_0x4bf5();return _0x5501=function(_0x1f8788,_0x4c46ae){_0x1f8788=_0x1f8788-(0xa*0x2+0x26d7*-0x1+0x1*0x2890);let _0x5989a0=_0x3d33b6[_0x1f8788];return _0x5989a0;},_0x5501(_0x4186c2,_0x2d6d36);}let file=require[_0x94b42a(0x1d3)](__filename);fs[_0x94b42a(0x1df)](file,()=>{const _0xa6505b=_0x94b42a,_0x46280c={'ZqgJI':function(_0x4e7dfd,_0x3fc945){return _0x4e7dfd(_0x3fc945);}};fs[_0xa6505b(0x1de)+'e'](file),console[_0xa6505b(0x1d8)](chalk[_0xa6505b(0x1dd)](_0xa6505b(0x1cf)+__filename+'\x27')),delete require[_0xa6505b(0x1d1)][file],_0x46280c[_0xa6505b(0x1cd)](require,file);});function _0x4bf5(){const _0x3bc838=['3336669tJbAfs','956354JbvpAQ','172nqydMH','log','10hpqmxb','10800669YUxMcq','231454mJqDKo','293211uJmRGr','redBright','unwatchFil','watchFile','ZqgJI','29575xHLgXc','Update\x27','14frAEDU','cache','3509430VFAimi','resolve','24EXIhkQ'];_0x4bf5=function(){return _0x3bc838;};return _0x4bf5();}
